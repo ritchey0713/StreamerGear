cce35b4d7e03b57bebcf8173d00fc212e32ba654
Dj <ritchey0713@gmail.com>
2020-03-17 19:47:43 -0400

    add linkurl

--- src/components/directory/directory.jsx
+++ src/components/directory/directory.jsx
@@ -9,29 +9,34 @@ class Directory extends Component {
         {
           title: "Hats",
           imageUrl: "https://images.unsplash.com/flagged/photo-1576533071143-28014b3839cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-          id: 1
+          id: 1,
+          linkUrl: "hats"
         },
         {
           title: "Sneakers",
           imageUrl: "https://images.unsplash.com/photo-1552346154-21d32810aba3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-          id: 4
+          id: 4,
+          linkUrl: "sneakers"
         },
         {
           title: "Jackets",
           imageUrl: "https://images.unsplash.com/photo-1521223890158-f9f7c3d5d504?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-          id: 5
+          id: 5,
+          linkUrl: "jackets"
         }, 
         {
           title: "Mens",
           imageUrl: "https://images.unsplash.com/photo-1581397867105-8c988bd747e6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
           size: 'large',
-          id: 2
+          id: 2, 
+          linkUrl: "mens"
         },
         {
           title: "Womans",
           imageUrl: "https://images.unsplash.com/photo-1573658070990-e4c084b95cc5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
           size: 'large',
-          id: 3
+          id: 3,
+          linkUrl: "womans"
         },
       ]
     }
@@ -41,8 +46,8 @@ class Directory extends Component {
     return(
       <div className="directory-menu">
         {
-          this.state.sections.map(({ title, imageUrl, id, size }) => (
-            <MenuItem key={ id } title={ title } imageUrl={ imageUrl } size={ size }/>
+          this.state.sections.map(({ id, ...otherSectionProps }) => (
+            <MenuItem key={ id } {...otherSectionProps}/>
           ))
         }
       </div>
