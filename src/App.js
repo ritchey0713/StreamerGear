4baf231fd48b9fa383de9cc437d61aff344c075f
Dj <ritchey0713@gmail.com>
2020-03-17 19:47:54 -0400

    add router switch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import HomePage from './pages/homepage';
+import { Switch, Route } from 'react-router-dom';
 
 const App = () => {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HomePage} />
+      </Switch>
     </div>
   );
 }
