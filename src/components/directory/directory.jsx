8f40c1738dc5fc8dd65eb1965548eebaf3f714c5
Dj <ritchey0713@gmail.com>
2020-03-17 13:47:17 -0400

    render menu items from state

--- src/components/directory/directory.jsx
+++ src/components/directory/directory.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react"
+import MenuItem from "../menuItem"
 
 class Directory extends Component {
   constructor(){
@@ -11,6 +12,16 @@ class Directory extends Component {
           id: 1
         },
         {
+          title: "Sneakers",
+          ImageUrl: "https://images.unsplash.com/photo-1552346154-21d32810aba3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+          id: 4
+        },
+        {
+          title: "Jackets",
+          ImageUrl: "https://images.unsplash.com/photo-1521223890158-f9f7c3d5d504?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+          id: 5
+        }, 
+        {
           title: "Mens",
           ImageUrl: "https://images.unsplash.com/photo-1581397867105-8c988bd747e6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
           id: 2
@@ -20,23 +31,21 @@ class Directory extends Component {
           ImageUrl: "https://images.unsplash.com/photo-1573658070990-e4c084b95cc5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
           id: 3
         },
-        {
-          title: "Sneakers",
-          ImageUrl: "https://images.unsplash.com/photo-1552346154-21d32810aba3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-          id: 4
-        },
-        {
-          title: "Jackets",
-          ImageUrl: "https://images.unsplash.com/photo-1521223890158-f9f7c3d5d504?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-          id: 5
-        }
       ]
     }
   }
   
   render(){
     return(
-
+      <div className="directory-menu">
+        {
+          this.state.sections.map(({ title, ImageUrl, id }) => (
+            <MenuItem key={ id } title={ title }/>
+          ))
+        }
+      </div>
     )
   }
 }
+
+export default Directory
