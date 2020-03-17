965db7e56902fa3cc3721318e0861f53c8e94b0d
Dj <ritchey0713@gmail.com>
2020-03-17 13:47:25 -0400

    remove basic html

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,51 +1,8 @@
 import React from "react"
+import Directory from "../components/directory/directory"
 
 const HomePage = () => (
-  <div className="homepage">
-    <div className="directory-menu">
-
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">Hats</h1>
-          <span className="subtitle">Shop Now</span>
-        </div>
-      </div>
-
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">Sneakers</h1>
-          <span className="subtitle">Shop Now</span>
-        </div>
-      </div>
-
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">Womans</h1>
-          <span className="subtitle">Shop Now</span>
-        </div>
-      </div>
-
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">Mens</h1>
-          <span className="subtitle">Shop Now</span>
-        </div>
-      </div>
-
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">Jackets</h1>
-          <span className="subtitle">Shop Now</span>
-        </div>
-      </div>
-
-    </div>
-  </div>
-
-
-
-
-
+  <Directory />
 )
 
 export default HomePage
