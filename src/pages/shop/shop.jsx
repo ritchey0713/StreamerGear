031940f55238e57e6cbaca3056c2f8187534375c
Dj <ritchey0713@gmail.com>
2020-03-18 16:12:45 -0400

    set shop data as collection

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,15 +1,20 @@
 import React, { Component } from 'react'
+import SHOP_DATA from "./shop.data"
 
 class ShopPage extends Component {
   constructor(props) {
     super(props)
-    
+    this.state = {
+      collections: SHOP_DATA
+    }
   }
   render(){
     return (
       <div>
-        
+
       </div>
     )
   }
 }
+
+export default ShopPage
