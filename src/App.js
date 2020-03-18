5eacf086856326bb237e81d093fe4606e8ae03df
Dj <ritchey0713@gmail.com>
2020-03-18 16:13:10 -0400

    set shop page routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage';
+import ShopPage from "./pages/shop/shop" 
 import { Switch, Route } from 'react-router-dom';
 
 const App = () => {
@@ -7,7 +8,7 @@ const App = () => {
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/hats" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
