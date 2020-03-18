bd0691b12b2c3f3add2067d0cd87ce9ff515647b
Dj <ritchey0713@gmail.com>
2020-03-18 16:13:01 -0400

    add in preview function

---
+++ src/components/collection-preview/collection-preview.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+
+const CollectionPreview = ({ title, items }) => (
+  <div className="collection-preview">
+    <h1 className="title">{ title.toUpperCase() }</h1>
+    <div className="preview">
+      {
+        items.map((item) => (
+          <div>
+            { item.name }
+          </div>
+        ))
+      }
+    </div>
+  </div>
+)
+
+export default CollectionPreview
