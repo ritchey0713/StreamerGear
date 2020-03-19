136b34b53cb86f6e5c8910bf246a6dacc4f56d70
Dj <ritchey0713@gmail.com>
2020-03-19 14:26:42 -0400

    build signin route

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import HomePage from './pages/homepage/homepage';
 import ShopPage from "./pages/shop/shop" 
 import { Switch, Route } from 'react-router-dom';
-import Header from './components/header/header';
+import Header from "./components/header/Header"
+import SignInLogIn from "./pages/signin-login/SignInLogIn"
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInLogIn} />
       </Switch>
     </div>
   );
