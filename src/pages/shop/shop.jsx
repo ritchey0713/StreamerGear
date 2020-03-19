5feb0c5f7c9217ea965ad9bd73f7abe6654c4ef4
Dj <ritchey0713@gmail.com>
2020-03-19 14:26:29 -0400

    fix whitespace

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -14,7 +14,7 @@ class ShopPage extends Component {
     return (
       <div className='shop-page'>
         {
-          collections.map(({ id, ... otherCollectionProps }) => (
+          collections.map(({ id, ...otherCollectionProps }) => (
             <CollectionPreview key={ id } { ...otherCollectionProps }/>
           ))
         }
