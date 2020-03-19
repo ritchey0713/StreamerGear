43e4fbb8fa516b4ea19cccb625ca01498cc92d3d
Dj <ritchey0713@gmail.com>
2020-03-19 13:32:58 -0400

    load header to pages

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import HomePage from './pages/homepage/homepage';
 import ShopPage from "./pages/shop/shop" 
 import { Switch, Route } from 'react-router-dom';
+import Header from './components/header/header';
 
 const App = () => {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
