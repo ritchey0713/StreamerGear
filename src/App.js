5eaa5e91e73f71c0a053cc76ce88dddb2dc19863
Dj <ritchey0713@gmail.com>
2020-03-23 12:11:04 -0400

    create class for currentuser state

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import HomePage from './pages/homepage/homepage';
 import ShopPage from "./pages/shop/shop" 
 import { Switch, Route } from 'react-router-dom';
 import Header from "./components/header/Header"
 import SignInLogIn from "./pages/signin-login/SignInLogIn"
+import { auth } from "./firebase/firebase"
 
-const App = () => {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/shop" component={ShopPage} />
-        <Route path="/signin" component={SignInLogIn} />
-      </Switch>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super()
+
+    this.state = {
+      currentUser: null
+    }
+  }
+  
+  render(){
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route path="/signin" component={SignInLogIn} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
