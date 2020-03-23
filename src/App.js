f4ecf1c86e48fbc6f4f0362c5e4dab7cb9f85625
Dj <ritchey0713@gmail.com>
2020-03-23 12:37:24 -0400

    add mounting

--- src/App.js
+++ src/App.js
@@ -14,11 +14,24 @@ class App extends Component {
       currentUser: null
     }
   }
-  
+
+  componentDidMount(){
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({
+        currentUser: user
+      })
+      console.log(user)
+    })
+  }
+
+  componentWillUnmount(){
+    this.unsubscribeFromAuth();
+  }
+
   render(){
     return (
       <div>
-        <Header />
+        <Header currentUser={this.state.currentUser}/>
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
