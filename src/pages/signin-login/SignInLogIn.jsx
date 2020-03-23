985ab6062afbcb5fd20e2d56cc55dd7cfecc2f8a
Dj <ritchey0713@gmail.com>
2020-03-23 14:44:05 -0400

    change import loaction

--- src/pages/signin-login/SignInLogIn.jsx
+++ src/pages/signin-login/SignInLogIn.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import SignIn from '../../components/sign-in/sign-in'
+import SignIn from '../../components/sign-in/signIn'
 
 const SignInLogInPage = () => (
   <div className="sign-in-log-in">
