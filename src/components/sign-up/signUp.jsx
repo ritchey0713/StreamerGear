53ce5e8ed6d3c9b8ed3ff5a859b4e97969d01fbd
Dj <ritchey0713@gmail.com>
2020-03-23 14:44:12 -0400

    create signUp component

---
+++ src/components/sign-up/signUp.jsx
@@ -0,0 +1,4 @@
+import React from 'react'
+import FormInput from '../form-input/form-input'
+import CustomButton from '../custom-button/CustomButton'
+import { auth, createUserProfileDocument } from '../../firebase/firebase'
