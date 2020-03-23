7f8c00545413c982785e1ee41e9ab95daf0ef5ff
Dj <ritchey0713@gmail.com>
2020-03-23 12:37:10 -0400

    add sign in/sign out links

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { ReactComponent as Logo } from "../../assets/logo.svg"
+import { auth } from "../../firebase/firebase"
 
 
-const Header = () => (
+const Header = ({ currentUser }) => (
   <div className="header">
     <Link to="/" className="logo-container">
       <Logo className="logo" />
@@ -17,6 +18,12 @@ const Header = () => (
       <Link to="/contact" className="option">
         CONTACT
       </Link>
+      {
+        currentUser ? 
+        <div className="option" onClick={() => auth.signOut()}>Sign out</div>
+        :
+        <Link className="option" to="/signin">SIGN IN</Link>
+      }
 
     </div>
   </div>
