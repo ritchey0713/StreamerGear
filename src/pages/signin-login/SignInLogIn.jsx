cbf88c43540faabf980c3cd26bf7a7a8b13c2324
Dj <ritchey0713@gmail.com>
2020-03-24 11:48:12 -0400

    add sign up component

--- src/pages/signin-login/SignInLogIn.jsx
+++ src/pages/signin-login/SignInLogIn.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import SignIn from '../../components/sign-in/signIn'
+import SignUp from '../../components/sign-up/signUp'
 
 const SignInLogInPage = () => (
   <div className="sign-in-log-in">
     <SignIn />
+    <SignUp />
   </div>
 )
 
