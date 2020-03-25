d1dcec756f4655d25908ce5654257e37d542aed0
Dj <ritchey0713@gmail.com>
2020-03-25 13:55:16 -0400

    remove app state

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ class App extends Component {
   render(){
     return (
       <div>
-        <Header currentUser={this.state.currentUser}/>
+        <Header />
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
