88befbb87dd70a614abf1bba7a1f4bab17aa1c87
Dj <ritchey0713@gmail.com>
2020-03-26 12:14:27 -0400

    build cart icon component

---
+++ src/components/cart-icon/cart-icon.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import { ReactComponent as ShoppingIcon } from "../../assets/cart.svg"
+
+const CartIcon = () => (
+  <div className="cart-icon">
+    <ShoppingIcon  className="shopping-icon"/>
+    <span className="item-count">0</span>
+  </div>
+)
+
+export default CartIcon
