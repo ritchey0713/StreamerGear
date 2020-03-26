91a2c6ad3555f373535304d355ebfd61f198373a
Dj <ritchey0713@gmail.com>
2020-03-26 12:40:23 -0400

    fix class name

--- src/components/cart-dropdown/cartDropdown.jsx
+++ src/components/cart-dropdown/cartDropdown.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import CustomButton from "../custom-button/CustomButton"
 
-const cartDropDown = () => (
-  <div className="care-dropdown">
+const CartDropDown = () => (
+  <div className="cart-dropdown">
     <div className="cart-items" />
     <CustomButton>GO TO CHECKOUT</CustomButton>
   </div>
 )
 
-export default cartDropDown
+export default CartDropDown
