181a81059d854681ac0861f74a025bdc7ab84a63
Dj <ritchey0713@gmail.com>
2020-03-26 18:11:02 -0400

    add custom button

--- src/components/collection-item/collection-item.jsx
+++ src/components/collection-item/collection-item.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import CustomButton from "../custom-button/CustomButton"
 
 const CollectionItem = ({ id, name, price, imageUrl }) => (
   <div className="collection-item">
@@ -9,6 +10,7 @@ const CollectionItem = ({ id, name, price, imageUrl }) => (
         <span className="name">{ name }</span>
         <span className="price">{ price }</span>
       </div>
+      <CustomButton inverted={true} itemPageButton={true}>ADD TO CART</CustomButton>
   </div>
 )
 
