d2b1a47424b5e435068cd9e913d81ad78c4a8aa1
Dj <ritchey0713@gmail.com>
2020-03-26 15:10:48 -0400

    add toggle event to icon

--- src/components/cart-icon/cart-icon.jsx
+++ src/components/cart-icon/cart-icon.jsx
@@ -1,11 +1,15 @@
 import React from "react"
+import { connect } from 'react-redux'
 import { ReactComponent as ShoppingIcon } from "../../assets/cart.svg"
+import { toggleCartHidden } from "../../redux/cart/cartActions"
 
-const CartIcon = () => (
-  <div className="cart-icon">
+const CartIcon = ({ toggleCartHidden }) => (
+  <div className="cart-icon" onClick={toggleCartHidden}>
     <ShoppingIcon  className="shopping-icon"/>
     <span className="item-count">0</span>
   </div>
 )
 
-export default CartIcon
+export default connect(undefined, {
+  toggleCartHidden
+})(CartIcon)
