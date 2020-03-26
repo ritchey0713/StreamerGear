d9083cac07c0e5b6f920cbabe3f94b4c2c119365
Dj <ritchey0713@gmail.com>
2020-03-26 12:14:38 -0400

    build cart drop down component

---
+++ src/components/cart-dropdown/cartDropdown.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import CustomButton from "../custom-button/CustomButton"
+
+const cartDropDown = () => (
+  <div className="care-dropdown">
+    <div className="cart-items" />
+    <CustomButton>GO TO CHECKOUT</CustomButton>
+  </div>
+)
+
+export default cartDropDown
