d4c1f141035f40ce79bbb9135019ba90d04352bc
Dj <ritchey0713@gmail.com>
2020-03-30 10:30:35 -0400

    restructure for redux action

--- src/components/collection-item/collection-item.jsx
+++ src/components/collection-item/collection-item.jsx
@@ -1,17 +1,25 @@
 import React from "react"
+import { connect } from 'react-redux'
 import CustomButton from "../custom-button/CustomButton"
+import { addItem } from "../../redux/cart/cartActions"
 
-const CollectionItem = ({ id, name, price, imageUrl }) => (
-  <div className="collection-item">
-    <div className="image"
-      style={{backgroundImage: `url(${imageUrl})`}}
-    />
-      <div className="collection-footer">
-        <span className="name">{ name }</span>
-        <span className="price">{ price }</span>
-      </div>
-      <CustomButton inverted={true} itemPageButton={true}>ADD TO CART</CustomButton>
-  </div>
-)
+const CollectionItem = ({ item, addItem }) => {
+  const {name, price, imageUrl } = item
 
-export default CollectionItem
+  return(
+    <div className="collection-item">
+      <div className="image"
+        style={{backgroundImage: `url(${imageUrl})`}}
+      />
+        <div className="collection-footer">
+          <span className="name">{ name }</span>
+          <span className="price">{ price }</span>
+        </div>
+        <CustomButton onClick={() => addItem(item)} inverted={true} itemPageButton={true}>ADD TO CART</CustomButton>
+    </div>
+  )
+}
+
+export default connect(undefined, {
+  addItem
+})(CollectionItem)
