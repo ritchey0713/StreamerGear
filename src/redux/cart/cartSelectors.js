0fbefb9a2ed2e0d95cd496d4632e19b3d4e5c926
Dj <ritchey0713@gmail.com>
2020-03-30 12:25:39 -0400

    add hidden selector

--- src/redux/cart/cartSelectors.js
+++ src/redux/cart/cartSelectors.js
@@ -8,10 +8,15 @@ import { createSelector } from "reselect"
 const selectCart = state => state.cart
 
 
-//pass back state as we need it, memoize happens, needs 2 args, a collection, and a "what to do with each from collection"
+//pass back state as we need it, memoize happens, needs 2 args, a collection, and a "what to do with each from collection"/ what to return
 export const selectCartItems = createSelector(
   [selectCart],
   (cart) => cart.cartItems
+)
+
+export const selectCartHidden = createSelector(
+  [selectCart],
+  cart => cart.hidden
 )
 
 export const selectCartItemsCount = createSelector(
