d0fc714f22749141bc5f67d19aa76ca936bb5599
Dj <ritchey0713@gmail.com>
2020-03-31 12:59:41 -0400

    add cart total selector

--- src/redux/cart/cartSelectors.js
+++ src/redux/cart/cartSelectors.js
@@ -27,3 +27,12 @@ export const selectCartItemsCount = createSelector(
       acc + cartItem.quantity, 
   0)
 )
+
+export const selectCartTotal = createSelector(
+  [selectCartItems],
+  cartItems => 
+  cartItems.reduce(
+    (acc, cartItem) => 
+      acc + (cartItem.quantity * cartItem.price), 
+  0)
+)
