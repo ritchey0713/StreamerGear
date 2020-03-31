9e573ee929fe114796a4286b4fc8d9606ebdbe23
Dj <ritchey0713@gmail.com>
2020-03-31 14:40:16 -0400

    add decrement util action

--- src/redux/cart/cartUtils.js
+++ src/redux/cart/cartUtils.js
@@ -11,3 +11,18 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
 
   return [...cartItems, {...cartItemToAdd, quantity: 1}]
 }
+
+export const removeItemFromCart = (cartItems, itemToRemove) => {
+  const existingCartItem = cartItems.find(cartItem => cartItem.id === itemToRemove.id)
+
+  if(existingCartItem.quantity === 1) {
+    return cartItems.filter(item => item.id !== itemToRemove.id)
+  }
+
+  return cartItems.map(item => 
+    item.id === itemToRemove.id ? 
+      {...item, quantity: item.quantity - 1} 
+      :
+      item
+  )
+}
