ef02f7689b0795f84a1e4e4e0bf69f96b3c02f10
Dj <ritchey0713@gmail.com>
2020-03-31 14:40:30 -0400

    add new reducer cases

--- src/redux/cart/cartReducer.js
+++ src/redux/cart/cartReducer.js
@@ -1,5 +1,5 @@
 import { cartActionTypes } from "./cartTypes"
-import { addItemToCart } from "./cartUtils"
+import { addItemToCart, removeItemFromCart } from "./cartUtils"
 
 const INITIAL_STATE = {
   hidden: true,
@@ -18,11 +18,16 @@ const cartReducer = (state = INITIAL_STATE, action) => {
         ...state,
         cartItems: addItemToCart(state.cartItems, action.payload)
       }
-    case cartActionTypes.REMOVE_ITEM: 
+    case cartActionTypes.CLEAR_ITEM_FROM_CART: 
     return {
       ...state,
       cartItems: state.cartItems.filter(item => item.id !== action.payload.id)
     }
+    case cartActionTypes.REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: removeItemFromCart(state.cartItems, action.payload)
+      }
     default: 
       return state
   }
