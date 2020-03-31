e6214b975e194134180aea65c51ec0f1f7b4cc99
Dj <ritchey0713@gmail.com>
2020-03-31 13:32:04 -0400

    add item render

--- src/components/checkout-item/checkout-item.jsx
+++ src/components/checkout-item/checkout-item.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
 
-const CheckoutItem = () => (
-  <div>
+const CheckoutItem = ({ cartItem: { name, imageUrl, price, quantity }}) => (
+  <div className="checkout-item">
+    <div className="image-container">
+      <img src={ imageUrl } alt="item"/>
+    </div>
+    <span className="name">{ name }</span>
+    <span className="quantity">{ quantity }</span>
+    <span className="price">{ price }</span>
+    <div className="remove-button">&#10008;</div>
   </div>
 )
 
