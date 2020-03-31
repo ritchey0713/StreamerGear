0cec1f7296f0c0ff610cbabb974e5e9885a58d74
Dj <ritchey0713@gmail.com>
2020-03-31 13:56:45 -0400

    wire remove item dispatch

--- src/components/checkout-item/checkout-item.jsx
+++ src/components/checkout-item/checkout-item.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { removeItem } from "../../redux/cart/cartActions"
 
-const CheckoutItem = ({ cartItem: { name, imageUrl, price, quantity }}) => (
-  <div className="checkout-item">
-    <div className="image-container">
-      <img src={ imageUrl } alt="item"/>
+const CheckoutItem = ({ cartItem, removeItem }) => {
+  const { imageUrl, name, quantity, price } = cartItem
+  return(
+    <div className="checkout-item">
+      <div className="image-container">
+        <img src={ imageUrl } alt="item"/>
+      </div>
+      <span className="name">{ name }</span>
+      <span className="quantity">{ quantity }</span>
+      <span className="price">{ price }</span>
+      <div className="remove-button" onClick={ () => removeItem(cartItem) }>&#10008;</div>
     </div>
-    <span className="name">{ name }</span>
-    <span className="quantity">{ quantity }</span>
-    <span className="price">{ price }</span>
-    <div className="remove-button">&#10008;</div>
-  </div>
-)
+  )
 
-export default CheckoutItem
+}
+
+export default connect(undefined, {
+  removeItem
+})(CheckoutItem)
