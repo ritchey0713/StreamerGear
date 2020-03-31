e923bcbaa33c72e48dd31f07073b4c2704d973f3
Dj <ritchey0713@gmail.com>
2020-03-31 14:39:44 -0400

    add arrows to increment quantity

--- src/components/checkout-item/checkout-item.jsx
+++ src/components/checkout-item/checkout-item.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { removeItem } from "../../redux/cart/cartActions"
+import { removeItem, clearItem, addItem } from "../../redux/cart/cartActions"
 
-const CheckoutItem = ({ cartItem, removeItem }) => {
+const CheckoutItem = ({ cartItem, removeItem, clearItem, addItem }) => {
   const { imageUrl, name, quantity, price } = cartItem
   return(
     <div className="checkout-item">
@@ -10,14 +10,20 @@ const CheckoutItem = ({ cartItem, removeItem }) => {
         <img src={ imageUrl } alt="item"/>
       </div>
       <span className="name">{ name }</span>
-      <span className="quantity">{ quantity }</span>
+      <span className="quantity">
+        <div className="arrow">&#10094;</div>
+        <span className="value">{ quantity }</span>
+        <div className="arrow" onClick={() => addItem(cartItem)}>&#10095;</div>
+      </span>
       <span className="price">{ price }</span>
-      <div className="remove-button" onClick={ () => removeItem(cartItem) }>&#10008;</div>
+      <div className="remove-button" onClick={ () => clearItem(cartItem) }>&#10008;</div>
     </div>
   )
 
 }
 
 export default connect(undefined, {
+  clearItem,
+  addItem,
   removeItem
 })(CheckoutItem)
