0e05d112371a4b7ef523c1e6e80791679b89a6c5
Dj <ritchey0713@gmail.com>
2020-04-06 10:43:37 -0400

    update method to not use find

--- src/redux/shop/shopSelectors.js
+++ src/redux/shop/shopSelectors.js
@@ -1,13 +1,5 @@
 import { createSelector } from "reselect"
 
-const COLLECTION_ID_MAP = {
-  hats: 1,
-  sneakers: 2,
-  jackets: 3,
-  womens: 4,
-  mens: 5
-}
-
 const selectShop = state => state.shop
 
 export const selectShopItems = createSelector(
@@ -19,6 +11,7 @@ export const selectShopItems = createSelector(
 export const selectCollection = collectionUrlParam => (
   createSelector(
     [selectShopItems],
-    collections => collections.find(collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam])
+    // collections => collections.find(collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam])
+    collections => collections[collectionUrlParam]
   )
 )
