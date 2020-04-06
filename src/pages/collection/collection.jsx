7e68c94f042877410f86cfc52abe33c0082b7ef5
Dj <ritchey0713@gmail.com>
2020-04-06 10:19:17 -0400

    update shop collention

---
+++ src/pages/collection/collection.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { connect } from "react-redux"
+import { selectCollection } from "../../redux/shop/shopSelectors"
+
+import CollectionItem from "../../components/collection-item/collection-item"
+
+const CollectionPage = ({ match, collection }) => {
+  const { title, items } = collection
+  return(
+  <div className="category">
+    <h2 className="title">{ title }</h2>
+    <div className="items">
+      {
+        items.map((item) => (
+          <CollectionItem  key={item.id} item={item} />
+        ))
+      }
+    </div>
+  </div>
+
+  )
+}
+
+const mapStateToProps = (state, ownProps) => ({
+  // if passing data to func in a selector, must also pass state in secondary call
+  collection: selectCollection(ownProps.match.params.collectionId)(state)
+})
+
+export default connect(mapStateToProps)(CollectionPage)
