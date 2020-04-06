e5274be68932aee0de13677545eb01bae75fff79
Dj <ritchey0713@gmail.com>
2020-04-06 10:43:14 -0400

    normalize url

--- src/redux/directory/directoryReducer.js
+++ src/redux/directory/directoryReducer.js
@@ -4,33 +4,33 @@ const INITIAL_STATE = {
       title: "Hats",
       imageUrl: "https://images.unsplash.com/flagged/photo-1576533071143-28014b3839cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
       id: 1,
-      linkUrl: "hats"
+      linkUrl: "shop/hats"
     },
     {
       title: "Sneakers",
       imageUrl: "https://images.unsplash.com/photo-1552346154-21d32810aba3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
       id: 4,
-      linkUrl: "sneakers"
+      linkUrl: "shop/sneakers"
     },
     {
       title: "Jackets",
       imageUrl: "https://images.unsplash.com/photo-1521223890158-f9f7c3d5d504?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
       id: 5,
-      linkUrl: "jackets"
+      linkUrl: "shop/jackets"
     }, 
     {
       title: "Mens",
       imageUrl: "https://images.unsplash.com/photo-1581397867105-8c988bd747e6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
       size: 'large',
       id: 2, 
-      linkUrl: "mens"
+      linkUrl: "shop/mens"
     },
     {
-      title: "Womans",
+      title: "Womens",
       imageUrl: "https://images.unsplash.com/photo-1573658070990-e4c084b95cc5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
       size: 'large',
       id: 3,
-      linkUrl: "womans"
+      linkUrl: "shop/womens"
     },
   ]
 }
