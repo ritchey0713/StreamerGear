79318eee75a5a4967d116e9222e3fcadd5ab7b0f
Dj <ritchey0713@gmail.com>
2020-04-13 11:35:03 -0400

    add stripe

--- src/index.js
+++ src/index.js
@@ -7,16 +7,20 @@ import { Provider } from 'react-redux'
 import { store, persistor } from "./redux/store"
 
 import { PersistGate } from 'redux-persist/integration/react'
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js'
 
 
 
 ReactDOM.render(
   <Provider store={store}>
+
     <BrowserRouter>
       <PersistGate persistor={persistor}>
         <App /> 
       </PersistGate>
     </BrowserRouter>
+
   </Provider>,
   document.getElementById('root'));
 
