fee2ee56970393716682c5fdcad565b5d844dfa2
Dj <ritchey0713@gmail.com>
2020-04-13 11:35:21 -0400

    add stripe button checkout

---
+++ src/components/stripe-button/StripeButton.jsx
@@ -0,0 +1,33 @@
+import React from "react"
+import StripeCheckout from 'react-stripe-checkout'
+import { CardElement} from "@stripe/react-stripe-js"
+
+const StripeCheckoutButton = ({ price, stripe, elements }) => {
+  const stripePrice = price * 100
+  const publishKey = 'pk_test_b2T1okYCs7N8suRfyieZ12aX00UFOXSVlF'
+
+  const onToken = token => {
+    console.log(token)
+    alert("Payment success!")
+  }
+
+  return (
+    <StripeCheckout 
+      label="Pay Now"
+      name="Stream Gear"
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`your total is $${price}`}
+      amount={stripePrice}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishKey}
+    />
+
+    
+  )
+
+}
+
+export default StripeCheckoutButton
