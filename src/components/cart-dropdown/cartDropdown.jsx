f0591b7c05b44b6e878f1858f92288a2ef61c02c
Dj <ritchey0713@gmail.com>
2020-04-27 19:51:47 -0400

    refactor to use styled components

--- src/components/cart-dropdown/cartDropdown.jsx
+++ src/components/cart-dropdown/cartDropdown.jsx
@@ -8,27 +8,28 @@ import CartItem from '../cart-item/cartItem'
 import { selectCartItems } from "../../redux/cart/cartSelectors"
 import { toggleCartHidden } from "../../redux/cart/cartActions"
 import { createStructuredSelector } from 'reselect'
+import { CartDropdownContainer, CartItemsContainer, EmptyMessageContainer } from "./cartDropDown.styles"
 
 const CartDropDown = ({ cartItems, history, toggleCartHidden }) => (
-  <div className="cart-dropdown">
-    <div className="cart-items">
+  <CartDropdownContainer>
+    <CartItemsContainer>
       {
         cartItems.length ?
         (cartItems.map(item => 
           <CartItem key={item.id} item={item} />
         )
         ):(
-          <span className="empty-message">Your cart is empty</span>
+          <EmptyMessageContainer>Your cart is empty</EmptyMessageContainer>
 
         )
       }
-    </div>
+    </CartItemsContainer>
     <CustomButton onClick={ () => {
       history.push("/checkout")
       toggleCartHidden()
       } }>GO TO CHECKOUT</CustomButton>
     
-  </div>
+  </CartDropdownContainer>
 )
 
 const mapStateToProps = createStructuredSelector({
