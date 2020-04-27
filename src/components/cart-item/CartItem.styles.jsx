5681e34404c0bc9368b4f5a2df78dbae573a6397
Dj <ritchey0713@gmail.com>
2020-04-27 19:43:20 -0400

    create cart item styled components

---
+++ src/components/cart-item/CartItem.styles.jsx
@@ -0,0 +1,29 @@
+import styled from 'styled-components'
+
+export const CartItemContainer = styled.div`
+  width: 100%;
+  display: flex;
+  height: 80px;
+  margin-bottom: 15px;
+`
+
+export const CartImgContainer = styled.img`
+ width: 30%
+`
+
+export const ItemDetailsContainer = styled.div`
+  width: 70%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  margin: 10px;
+`
+
+export const ItemNameContainer = styled.span`
+  font-size: 16px;
+`
+export const ItemPriceContainer = styled.span`
+  width: 23%;
+  padding-left: 30px;
+`
