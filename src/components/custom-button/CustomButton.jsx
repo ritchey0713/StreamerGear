83227cc5e70a81c2b7b18e6e02354f58e94103cd
Dj <ritchey0713@gmail.com>
2020-04-27 11:34:42 -0400

    refactor down complexity of styles

--- src/components/custom-button/CustomButton.jsx
+++ src/components/custom-button/CustomButton.jsx
@@ -1,11 +1,19 @@
 import React from "react"
+import { CustomButtonContainer } from "./customButton.styles"
 
-const CustomButton = ({ children, isGoogleSignIn, itemPageButton, inverted, ...otherProps }) => (
-  <button className={`${inverted && 'inverted'} ${isGoogleSignIn && 'google-sign-in'} ${itemPageButton ? 'items-button custom-button' : 'custom-button'}`} 
-    { ...otherProps }
+// const CustomButton = ({ children, isGoogleSignIn, itemPageButton, inverted, ...otherProps }) => (
+//   <button className={`${inverted && 'inverted'} ${isGoogleSignIn && 'google-sign-in'} ${itemPageButton ? 'items-button custom-button' : 'custom-button'}`} 
+//     { ...otherProps }
+//   >
+//     { children }
+//   </button>
+// )
+const CustomButton = ({children, ...props}) => (
+  <CustomButtonContainer  
+    { ...props }
   >
     { children }
-  </button>
+  </CustomButtonContainer>
 )
 
 export default CustomButton
