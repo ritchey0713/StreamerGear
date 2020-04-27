98d6fe7f3fcc0360f00efdcf386c550461616052
Dj <ritchey0713@gmail.com>
2020-04-27 19:44:36 -0400

    add cart item styled components

--- src/components/cart-item/cartItem.jsx
+++ src/components/cart-item/cartItem.jsx
@@ -1,13 +1,18 @@
 import React from "react"
 
+import { CartImgContainer, CartItemContainer, ItemDetailsContainer, ItemPriceContainer, ItemNameContainer } from "./CartItem.styles"
+
 const CartItem = ({ item: { imageUrl, price, name, quantity } }) => (
-  <div className="cart-item">
-    <img className="cart-img" src={imageUrl} alt="item"/>
-    <div className="item-details">
-      <span className="item-name">{name}</span>
-      <span className="price">{quantity} x ${price}</span>
-    </div>
-  </div>
+  <CartItemContainer>
+    <CartImgContainer src={imageUrl} alt="item"/>
+    
+    <ItemDetailsContainer>
+
+      <ItemNameContainer>{name}</ItemNameContainer>
+      <ItemPriceContainer>{quantity} x ${price}</ItemPriceContainer>
+      
+    </ItemDetailsContainer>
+  </CartItemContainer>
 )
 
 export default CartItem
