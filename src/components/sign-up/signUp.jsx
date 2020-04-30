7be59c3ed6f9bf3a7471729208449d59a3698a87
Dj <ritchey0713@gmail.com>
2020-04-30 13:15:36 -0400

    load components

--- src/components/sign-up/signUp.jsx
+++ src/components/sign-up/signUp.jsx
@@ -3,6 +3,8 @@ import FormInput from '../form-input/form-input'
 import CustomButton from '../custom-button/CustomButton'
 import { auth, createUserProfileDocument } from '../../firebase/firebase'
 
+import { SignUpContainer, TitleContainer } from "./Signup.styles"
+
 class SignUp extends Component {
   constructor(){
     super()
@@ -49,10 +51,10 @@ class SignUp extends Component {
   render() {
     const { displayName, email, password, confirmPassword } = this.state
     return(
-      <div className="sign-up">
-        <h2 className="title">I do not have an account</h2>
+      <SignUpContainer>
+        <TitleContainer>I do not have an account</TitleContainer>
         <span>Sign up with your email!</span>
-        <form className="sign-up-form" onSubmit={this.handleSubmit}>
+        <form onSubmit={this.handleSubmit}>
           <FormInput
             type="text"
             name="displayName"
@@ -88,7 +90,7 @@ class SignUp extends Component {
           <CustomButton type="submit"> Sign up! </CustomButton>
 
         </form>
-      </div>
+      </SignUpContainer>
     )
   }
 }
