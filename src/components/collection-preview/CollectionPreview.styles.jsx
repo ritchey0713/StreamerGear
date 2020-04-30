c501c9acc9d0cf538b574fa6add6bdd9e293d0d9
Dj <ritchey0713@gmail.com>
2020-04-30 09:23:59 -0400

    add new styled components

---
+++ src/components/collection-preview/CollectionPreview.styles.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components"
+
+export const CollectionPreviewContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 30px;
+`
+
+export const titleContainer = styled.h1`
+  font-size: 30px;
+  margin-bottom: 25px;
+`
+
