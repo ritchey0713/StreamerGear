0f5f95e96efaf20bc1460a43a3f66c50d53570ba
Dj <ritchey0713@gmail.com>
2020-04-30 13:15:29 -0400

    add sign up styles

---
+++ src/components/sign-up/Signup.styles.jsx
@@ -0,0 +1,11 @@
+import styles from "styled-components"
+
+export const SignUpContainer = styles.div`
+  display: flex;
+  flex-direction: column;
+  width: 380px;
+`
+
+export const TitleContainer = styles.h2`
+  margin: 10px 0;
+`
