a281a5e01dae21d8300a2f978828e5e5af784d61
Dj <ritchey0713@gmail.com>
2020-05-01 11:44:01 -0400

    add item styles

--- src/components/collection-item/CollectionItem.styles.jsx
+++ src/components/collection-item/CollectionItem.styles.jsx
@@ -1,2 +1,29 @@
-import styles from 'styled-components'
+import styled from 'styled-components'
 
+export const CollectionItem = styled.div`
+width: 22vw;
+display: flex;
+flex-direction: column;
+height: 350px;
+align-items: center;
+position: relative;
+margin-bottom: 30px;
+`
+
+export const ImageContainer = styled.div`
+  width: 100%;
+  height: 95%;
+  background-size: cover;
+  background-position: center;
+  margin-bottom: 7px;
+`
+
+export const CollectionFooterContainer = styled.div`
+  width: 100%;
+  height: 5%;
+  display: flex;
+  justify-content: space-between;
+  font-size: 20px;
+`
+export const NameCollection = styled.span`
+`
