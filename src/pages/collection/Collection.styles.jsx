9c09ad465c8faf0ef5bb351d047754d9a5d6d9bd
Dj <ritchey0713@gmail.com>
2020-05-01 11:43:45 -0400

    create style file

---
+++ src/pages/collection/Collection.styles.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components"
+
+export const TitleContainer = styled.h2`
+  font-size: 38px;
+  margin: 0 auto 30px;
+`
+
+export const ItemsContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-gap: 10px;
+`
