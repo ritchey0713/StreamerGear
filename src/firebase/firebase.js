1eb985fc0f984fcb2845d481fe13be13e6866024
Dj <ritchey0713@gmail.com>
2020-05-04 14:12:15 -0400

    add func to add seed data

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -36,4 +36,9 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   return userRef
 }
 
+export const addCollectionAndDocuments = (collectionKey, objsToAdd) => {
+  const collectionRef = firestore.collection(collectionKey);
+  console.log(collectionRef)
+}
+
 export default firebase
