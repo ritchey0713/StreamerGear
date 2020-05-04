01c21ad5c22d6f1dea5558bf0fb87683167944f3
Dj <ritchey0713@gmail.com>
2020-05-04 08:28:09 -0400

    add new styles

--- src/pages/collection/collection.jsx
+++ src/pages/collection/collection.jsx
@@ -3,19 +3,20 @@ import { connect } from "react-redux"
 import { selectCollection } from "../../redux/shop/shopSelectors"
 
 import CollectionItem from "../../components/collection-item/collection-item"
+import { TitleContainer, ItemsContainer } from "./Collection.styles"
 
 const CollectionPage = ({ match, collection }) => {
   const { title, items } = collection
   return(
   <div>
-    <h2 className="title">{ title }</h2>
-    <div className="items">
+    <TitleContainer>{ title }</TitleContainer>
+    <ItemsContainer>
       {
         items.map((item) => (
           <CollectionItem  key={item.id} item={item} />
         ))
       }
-    </div>
+    </ItemsContainer>
   </div>
 
   )
