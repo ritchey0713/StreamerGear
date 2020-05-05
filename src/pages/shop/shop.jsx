e8eecde34a95aaa557a327f17e43ad1580850e75
Dj <ritchey0713@gmail.com>
2020-05-05 11:09:00 -0400

    add component did mount

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,14 +1,32 @@
-import React from 'react'
+// nested router file app renders this, this renders children routes
+// match comes from react-router 
+import React, { Component } from 'react'
 import { Route } from "react-router-dom"
 import CollectionsOverview from '../../components/collections-overview/CollectionsOverview'
 import CollectionPage from "../collection/collection"
+import { firestore, convertCollectionsSnapshotToMap } from "../../firebase/firebase" 
 
-const ShopPage = ({ match }) => (
+class ShopPage extends Component {
+  unsubscribeFromSnapshot = null;
+
+  componentDidMount() {
+    const collectionRef = firestore.collection("collections")
+
+    collectionRef.onSnapshot(async snapshot => {
+      convertCollectionsSnapshotToMap(snapshot)
+    })
+  }
+  render(){
+    const { match } = this.props
+    return (
       <div className='shop-page'>
         <Route exact path={`${match.path}`} component={CollectionsOverview} />
         <Route path={`${match.path}/:collectionId`} component={CollectionPage}/>
       </div>
-)
+
+    )
+  }
+} 
 
 
 export default ShopPage
