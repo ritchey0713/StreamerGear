662533443d8525b33a61c6541db27131e4639a44
Dj <ritchey0713@gmail.com>
2020-05-06 12:02:23 -0400

    create spinner HOC

--- src/components/with-spinner/WithSpinner.jsx
+++ src/components/with-spinner/WithSpinner.jsx
@@ -2,6 +2,17 @@ import React from "react"
 
 import { SpinnerContainer, SpinnerOverlay } from "./WithSpinner.styles"
 
-const WithSpinner = (wrappedComponent) => {
-
+const WithSpinner = (WrappedComponent) => {
+  const Spinner = ({isLoading, ...otherProps}) => {
+    return isLoading ? (
+      <SpinnerOverlay>
+        <SpinnerContainer />
+      </SpinnerOverlay>
+    ) : (
+      <WrappedComponent {...otherProps} />
+    )
+  }
+  return Spinner
 }
+
+export default WithSpinner
