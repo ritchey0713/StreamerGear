2fb522f0973664ff62bfd05ca36d4a3eca742008
Dj <ritchey0713@gmail.com>
2020-05-08 11:53:32 -0400

    build async action for fetching shop data

--- src/redux/shop/shopActions.js
+++ src/redux/shop/shopActions.js
@@ -1,6 +1,19 @@
 import ShopActionTypes from "./shop.types"
+import { firestore } from "../../firebase/firebase"
 
-export const updateCollections = (collectionsMap) => ({
-  type: ShopActionTypes.UPDATE_COLLECTIONS,
-  payload: collectionsMap
+
+export const fetchCollectionsStart = () => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_START,
 })
+
+export const fetchCollectionsStartAsync = () => {
+  return dispatch => {
+    const collectionRef = firestore.collection("collections")
+
+    collectionRef.get().then(async snapshot => {
+        const collectionsMap = convertCollectionsSnapshotToMap(snapshot)
+        //updateCollections(collectionsMap)
+        this.setState({loading: false})
+      })
+  }
+}
