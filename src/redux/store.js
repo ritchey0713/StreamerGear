020449e35114aefa954c764c3202c6d5cc10da0b
Dj <ritchey0713@gmail.com>
2020-05-13 11:50:50 -0400

    add redux tools

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,20 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import logger from 'redux-logger'
 import { persistStore } from 'redux-persist'
 import thunk from "redux-thunk"
 
 import rootReducer from "./rootReducer"
 
-const middlewares = [thunk]
+const middlewares = [thunk, logger]
 
-export const store = createStore(rootReducer, applyMiddleware(...middlewares))
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export const store = createStore(rootReducer, composeEnhancers(
+      applyMiddleware(...middlewares)
+    ));
+
+
+//export const store = createStore(rootReducer, applyMiddleware(...middlewares))
 
 export const persistor = persistStore(store)
 
