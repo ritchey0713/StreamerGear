90e8cec35b6f94c65145696dd8016bb1a5b2d151
Dj <ritchey0713@gmail.com>
2020-05-13 11:51:11 -0400

    reorder actions

--- src/redux/shop/shopActions.js
+++ src/redux/shop/shopActions.js
@@ -3,24 +3,8 @@ import { firestore, convertCollectionsSnapshotToMap } from "../../firebase/fireb
 
 
 export const fetchCollectionsStart = () => ({
-  type: ShopActionTypes.FETCH_COLLECTIONS_START,
+  type: ShopActionTypes.FETCH_COLLECTIONS_START
 })
-
-export const fetchCollectionsStartAsync = () => {
-  return dispatch => {
-    const collectionRef = firestore.collection("collections")
-    dispatch(fetchCollectionsStart())
-
-    collectionRef.get().then(snapshot => {
-        const collectionsMap = convertCollectionsSnapshotToMap(snapshot)
-        dispatch(fetchCollectionsSuccess(collectionsMap))
-        //updateCollections(collectionsMap)
-      })
-      .catch(
-        error => dispatch(fetchCollectionsError(error.message)) 
-      )
-  }
-}
 
 export const fetchCollectionsSuccess = (collectionsMap) => ({
   type: ShopActionTypes.FETCH_COLLECTIONS_SUCCESS,
@@ -29,5 +13,19 @@ export const fetchCollectionsSuccess = (collectionsMap) => ({
 
 export const fetchCollectionsError = (msg) => ({
   type: ShopActionTypes.FETCH_COLLECTIONS_FAILURE,
-  paylaod: msg
+  payload: msg
 })
+
+export const fetchCollectionsStartAsync = () => {
+  return dispatch => {
+    const collectionRef = firestore.collection("collections")
+    dispatch(fetchCollectionsStart())
+    collectionRef.get().then(snapshot => {
+        const collectionsMap = convertCollectionsSnapshotToMap(snapshot)
+        dispatch(fetchCollectionsSuccess(collectionsMap))
+      })
+      .catch(
+        error => dispatch(fetchCollectionsError(error.message)) 
+      )
+  }
+}
